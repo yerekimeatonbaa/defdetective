8af108b0e9c73327456abe58655c88ddd87f772d
Tonbra Benjamin Yerekimea <yerekimeatonbaa@gmail.com>
2025-11-08 19:23:13 +0000

    Try fixing this error: `Runtime Error: Error: An unexpected response was

--- src/ai/flows/game-sounds-flow.ts
+++ src/ai/flows/game-sounds-flow.ts
@@ -65,7 +65,7 @@ const gameSoundFlow = ai.defineFlow(
   },
   async query => {
     const {media} = await ai.generate({
-      model: googleAI.model('gemini-1.5-flash-preview-tts'),
+      model: googleAI.model('gemini-2.5-flash-preview-tts'),
       config: {
         responseModalities: ['AUDIO'],
         speechConfig: {
