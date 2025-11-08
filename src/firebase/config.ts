8bc64b906ef34121d665c8ae9cc02f6c148f7cba
Tonbra Benjamin Yerekimea <yerekimeatonbaa@gmail.com>
2025-11-08 19:19:28 +0000

    Try fixing this error: `Console Error: [2025-11-08T19:16:14.144Z] @fireb

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,9 @@
 export const firebaseConfig = {
-  "projectId": "studio-4536174912-ee6ca",
-  "appId": "1:835318680713:web:ed6b1fbf0a8684b8a08a58",
   "apiKey": "AIzaSyDdx1Qxmt89XJmQTLYuUw96E32_Yu07iPs",
   "authDomain": "studio-4536174912-ee6ca.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "835318680713"
+  "projectId": "studio-4536174912-ee6ca",
+  "storageBucket": "studio-4536174912-ee6ca.appspot.com",
+  "messagingSenderId": "835318680713",
+  "appId": "1:835318680713:web:ed6b1fbf0a8684b8a08a58",
+  "measurementId": "G-40J5T2N7G3"
 };
