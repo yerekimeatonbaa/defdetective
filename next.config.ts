7c715950a6fd1e04872845c8ee71c7c5cb70c5b1
Tonbra Benjamin Yerekimea <yerekimeatonbaa@gmail.com>
2025-11-09 14:13:47 +0000

    [2025-11-09T13:35:25.394Z]  @firebase/firestore: "Firestore (11.9.0): Co

--- next.config.ts
+++ next.config.ts
@@ -1,20 +1,8 @@
 
 import type {NextConfig} from 'next';
 
-// Load environment variables from .env file
-require('dotenv').config({ path: './.env' });
-
 const nextConfig: NextConfig = {
   /* config options here */
-  env: {
-    NEXT_PUBLIC_FIREBASE_API_KEY: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-    NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-    NEXT_PUBLIC_FIREBASE_PROJECT_ID: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-    NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-    NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-    NEXT_PUBLIC_FIREBASE_APP_ID: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-    NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
-  },
   typescript: {
     ignoreBuildErrors: true,
   },
